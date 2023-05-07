54d9ad6e35062235f47107436065b536172f2af0
Michał Skoczek <mwskoczek@gmail.com>
2023-05-07 23:28:58 +0200

    feat: form - basic questions implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BasicQuestionsModule } from './form/basic-questions/basic-questions.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeaderModule, NoopAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    ReactiveFormsModule,
+    BasicQuestionsModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
