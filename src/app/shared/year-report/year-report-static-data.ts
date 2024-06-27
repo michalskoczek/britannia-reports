3383165f141592905ae21da642fd76e6ac8d1642
Michał Skoczek <mwskoczek@gmail.com>
2024-06-27 23:23:22 +0200

    feat: update year report (short version)

---
+++ src/app/shared/year-report/year-report-static-data.ts
@@ -0,0 +1,38 @@
+export const parentDecisionValues: string[] = ['Tak', 'Nie', 'Brak'];
+
+export const certificationPurposeOnThisYear: string[] = [
+  'zrealizowany',
+  'niezrealizowany',
+];
+
+export const markEvaluations: string[] = [
+  'celujący',
+  'bardzo dobry',
+  'bardzo dobry –',
+  'dobry +',
+  'dobry',
+  'dobry -',
+  'dostateczny +',
+  'dostateczny',
+  'dostateczny -',
+  'dopuszczający +',
+  'dopuszczający',
+  'niedostateczny',
+];
+
+export const recommendationsInNextYear: string[] = [
+  'Fast Track',
+  'Regular Track',
+  'Support Track',
+];
+
+export const examsRecommendationsInTable: string[] = [
+  'Brak rekomendacji',
+  'Cambridge STARTERS',
+  'Cambridge MOVERS',
+  'Cambridge FLYERS',
+  'Cambridge A2 Key for Schools (KET)',
+  'Cambridge B1 Preliminary for Schools (PET)',
+  'Cambridge B2 First for Schools (FCE)',
+  'Cambridge C1 Advanced (CAE)',
+];
